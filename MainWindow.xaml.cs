1c224fda9d35323462507b79cc587faa4fc8acf3
rdegans <38084642+rdegans@users.noreply.github.com>
2019-02-11 08:57:08 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿/*Name:Riley de Gans
+ *Date:Feb. 8th, 2019
+ *description
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184863IPO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            decimal length, width, area;
+            decimal.TryParse(txtLength.Text, out length);
+            decimal.TryParse(txtLength.Text, out width);
+            area = length * width;
+            lblOutput.Content = "The area is: " + area.ToString("#.00");
+        }
+    }
+}
